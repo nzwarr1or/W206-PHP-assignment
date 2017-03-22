35642fffb977e99a7f6eebb6613a8e56cbe21802
nzwarr1or <SH064@ATCLTD.ATC.ORG.NZ>
2017-03-23 10:32:19 +1300

    Final
    finished

--- partials/thanks.php
+++ partials/thanks.php
@@ -11,7 +11,7 @@ include 'header.php';
 	<div class="row">
 		<div class="col s12 offset-3 m4 offset-l3 l4">
   		<h4> Thanks for the feedback <span><?= $name ?></span></h4>
-		<h4>We will reply to <span><?= $email ?></span> if we need to follow up.</h4>
+		<h4>We will reply to <span><?= $email ?></span><br>if we need to follow up.</h4>
 		<a href="index.php">Return to form</a>
 	</div>
   	</div>
